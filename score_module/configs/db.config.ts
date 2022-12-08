b8a68f6fc55b26b3ed8659dec02a8ce2e733ef36
Lilian-MMI <lilianouvmmi@gmail.com>
2022-12-08 14:53:04 +0100

    fix: update database env

--- score_module/configs/db.config.ts
+++ score_module/configs/db.config.ts
@@ -1,10 +1,9 @@
 module.exports = {
-  HOST:
-    process.env.DATABASE_HOST || "mysql-architecture-logicielle.alwaysdata.net",
-  USER: process.env.DATABASE_USER || "250570",
-  PASSWORD: process.env.DATABASE_PASSWORD || "tp_final_password",
-  DB: process.env.DATABASE_TABLE || "architecture-logicielle_bdd-3",
-  dialect: "mysql",
+  HOST: process.env.DATABASE_HOST || 'mysql-lilian-projects.alwaysdata.net',
+  USER: process.env.DATABASE_USER || '292277_quizzme',
+  PASSWORD: process.env.DATABASE_PASSWORD || 'quizzme_mdp',
+  DB: process.env.DATABASE_TABLE || 'lilian-projects_web-service-quizzme',
+  dialect: 'mysql',
   pool: {
     min: 0,
     acquire: 30000,
@@ -13,5 +12,5 @@ module.exports = {
   dialectOptions: {
     useUTC: false,
   },
-  timezone: "+01:00",
+  timezone: '+01:00',
 };
--- user_module/server.js
+++ user_module/server.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const usersRouter = require("./routes/user.routes");
-const morgan = require("morgan");
-const cors = require("cors");
+const express = require('express');
+const mongoose = require('mongoose');
+const usersRouter = require('./routes/user.routes');
+const morgan = require('morgan');
+const cors = require('cors');
 
 // Constants
 const PORT = 8080;
 const corsOptions = {
   origin: [
-    "http://0.0.0.0:8081",
-    "http://0.0.0.0:8082",
-    "http://localhost:3001",
+    'http://0.0.0.0:8081',
+    'http://0.0.0.0:8082',
+    'http://localhost:3001',
   ],
   credentials: true,
 };
@@ -18,13 +18,13 @@ const corsOptions = {
 // App
 const app = express();
 app.use(express.json());
-app.use(morgan("dev"));
-app.use("/users", usersRouter);
+app.use(morgan('dev'));
+app.use('/users', usersRouter);
 app.use(cors(corsOptions));
 
 const dbURI =
   process.env.DB_URI ||
-  "mongodb+srv://tp_final_username:tp_final_password@cluster.3udzt.mongodb.net/cluster?retryWrites=true&w=majority";
+  'mongodb+srv://tp_final_username:tp_final_password@cluster.3udzt.mongodb.net/cluster?retryWrites=true&w=majority';
 mongoose
   .connect(dbURI, {
     useNewUrlParser: true,
@@ -32,7 +32,7 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () =>
-      console.log("\x1b[33m%s\x1b[0m", `Server listen on port : ${PORT} 🚀`)
+      console.log('\x1b[33m%s\x1b[0m', `Server listen on port : ${PORT} 🚀`)
     );
   })
   .catch((err) => console.log(err));
