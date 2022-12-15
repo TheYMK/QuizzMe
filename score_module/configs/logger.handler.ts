dccbeded49729f51d7c5d0744f079c4432e4862a
Lilian-MMI <lilianouvmmi@gmail.com>
2022-12-15 16:03:59 +0100

    fix: binding port logger on 8083

--- score_module/configs/logger.handler.ts
+++ score_module/configs/logger.handler.ts
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const loggerHandler = async ({ message, level }) => {
-  await axios.post("http://172.20.0.5:8083/logger/log", {
+  await axios.post('http://172.20.0.5:8083/logger/log', {
     message,
     level,
-    module: "score",
+    module: 'score',
   });
 };
--- user_module/configs/logger.handler.js
+++ user_module/configs/logger.handler.js
@@ -1,7 +1,11 @@
 const axios = require('axios');
 
 const loggerHandler = async ({ message, level }) => {
-  await axios.post("http://172.20.0.5:8083/logger/log", { message, level, module: 'user' })
-}
+  await axios.post('http://172.20.0.5:8083/logger/log', {
+    message,
+    level,
+    module: 'user',
+  });
+};
 
 module.exports = loggerHandler;
